0072210e758139d16f2ba55f17930e5edfd89afa
Vinayak <64136105+vinig1305@users.noreply.github.com>
2023-04-21 15:32:32 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  AWS_Access_Key_ID     = "AKIASMHUO6FBNKLUPDPH"
-  AWS_Secret_Access_Key = "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"
-  region                = "us-west-2"
+  access_key = "AKIASMHUO6FBNKLUPDPH"
+  secret_key = "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"
+  region     = "us-east-1"
 }
