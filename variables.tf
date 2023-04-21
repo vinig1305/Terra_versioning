a8cb0825c6ab3bb3616357306e6f83e6f38cdcb2
Vinayak <vinayakbgoni@gmail.com>
2023-04-21 12:58:25 +0530

    updated the variables file

--- variables.tf
+++ variables.tf
@@ -1,1 +1,9 @@
 Terra_versioning
+# Define input variables for the EC2 instance
+variable "ami_id" {
+  description = "The ID of the AMI to use for the EC2 instance"
+}
+
+variable "instance_type" {
+  description = "The type of instance to launch"
+}
