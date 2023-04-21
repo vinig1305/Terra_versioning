bf7e2dbaea38a7a81056537f2d55a62cb2589a3b
Vinayak <vinayakbgoni@gmail.com>
2023-04-21 12:56:43 +0530

    updated the main.tf file

--- main.tf
+++ main.tf
@@ -1,1 +1,25 @@
 Terra_versioning
+# Define a single AWS EC2 instance
+resource "aws_instance" "example" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  tags = {
+    Name = "example-instance"
+  }
+}
+
+# Define a security group to allow inbound SSH traffic
+resource "aws_security_group" "ssh" {
+  name_prefix = "ssh-"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Associate the security group with the EC2 instance
+resource "aws_instance" "example" {
+  security_groups = [aws_security_group.ssh.id]
+}
